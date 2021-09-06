ef61b820e7d9e58e3c43e4caa62c86b6d0ce96a0
Pedro <pedrochatelain@gmail.com>
2021-09-06 19:21:31 -0300

    Botón filtro muestra filtros al ser clickeado

--- TP1/main.js
+++ TP1/main.js
@@ -1,2 +1,15 @@
-const canvas = document.querySelector('canvas');
-console.log(canvas.width);
+function init() {
+  const btnFilter = document.querySelector('.js-filter-button');
+  const filterPopUp = document.querySelector('.js-filters-pop-up');
+  
+  filterPopUp.addEventListener('click', () => console.log('clicked'))
+  btnFilter.addEventListener('click', () => showFilters(filterPopUp));
+  btnFilter.addEventListener('focusout', () => showFilters(filterPopUp));
+}
+
+function showFilters(popUp) {
+  popUp.classList.toggle('display-none');
+}
+
+
+init();
