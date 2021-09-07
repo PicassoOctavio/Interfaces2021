58fa6673b04f041f62313d00aa8d96b01083bea7
Picasso Octavio <octaviopicasso.ma@gmail.com>
2021-09-06 22:36:48 -0300

    funcion de dibujo/borrado + slider

--- TP1/main.js
+++ TP1/main.js
@@ -5,6 +5,7 @@ document.addEventListener("DOMContentLoaded", function() {
   let input = document.querySelector('.input1')
   let context = canvas.getContext('2d');
   let imageData = context.createImageData( canvas.width, canvas.height );
+  let color;
   
   //--------------------- pedro -----------------------------------------------
 
@@ -22,6 +23,26 @@ document.addEventListener("DOMContentLoaded", function() {
   }
   
   init();
+  //--------------------------------------------------------------------
+
+  //Cargando imagen de fondo local al iniciar
+  let image = new Image();
+  image.crossOrigin = 'Anonymous';
+  image.src = 'fondoInicio.jpg';
+  image.onload = function () {
+      context.fillStyle = "#F0F0EC"; // canvas background color
+      const imageAspectRatio = (1.0 * this.height) / this.width;
+      const imageScaledWidth = canvas.width;
+      const imageScaledHeight = canvas.width * imageAspectRatio;
+      context.fillRect(0, 0, canvas.width, canvas.height);
+      // draw image on canvas
+      context.drawImage(this, 0, 0, imageScaledWidth, imageScaledHeight);
+      // get imageData from content of canvas
+      const imageData = context.getImageData(0, 0, imageScaledWidth, imageScaledHeight);
+      // draw the modified image
+      context.putImageData(imageData, 0, 0);
+  }
+
   //--------------------------------------------------------------------
 
   //Cargar imagen adaptable
@@ -47,6 +68,96 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     }
   }
+  
+  //--------------------------------------------------------------------
+  //SLIDER PINCEL
+  let tamanioPincel = document.querySelector("#tamanioPincel");
+  let sliderValor = document.querySelector("#slider");
+  tamanioPincel.addEventListener("input", slider);
+
+  //------------------------------------------------------------------
+  // toma el valor del input (type = range) para darle el tamaño al pincel/borrador
+  function slider(){ 
+      // el span se vuelve el valor del input "tamanioPincel"
+      sliderValor.innerHTML = tamanioPincel.value;
+      return tamanioPincel.value;
+  };
+
+  //--------------------------------------------------------------------
+  let lapiz = document.querySelector('#lapiz');
+  lapiz.addEventListener('click', accionDibujo);
+
+  function accionDibujo() { // DIBUJO/BORRAR
+    
+    console.log("entro");
+
+    let prevX = 0;
+    let currX = 0;
+    let prevY = 0;
+    let currY = 0;
+    let dibujar = false;
+    let flag = false;        
+    color = "#000000";
+
+    canvas.addEventListener("mousemove", function (e) {
+        encontrarXy('move', e, color)
+    }, false);
+    canvas.addEventListener("mousedown", function (e) {
+        encontrarXy('down', e, color)
+    }, false);
+    canvas.addEventListener("mouseup", function (e) {
+        encontrarXy('up', e)
+    }, false);
+    canvas.addEventListener("mouseout", function (e) {
+        encontrarXy('out', e)
+    }, false);
+
+    //------------------------------------------------------------------------
+    function encontrarXy(res, e, color) {
+        console.log( "res" , res, "| e", e, " | color", color );
+
+        if ( res == 'down' ) {
+            prevX = currX;
+            prevY = currY;
+            currX = e.pageX - canvas.offsetLeft;
+            currY = e.pageY - canvas.offsetTop;
+            flag = true;
+            dibujar = true;
+
+            if ( dibujar ) {
+                context.beginPath();
+                context.arc(currX, currY, 7.5, Math.PI*2, false);
+                context.lineWidth = slider();
+                context.strokeStyle = color;
+                context.stroke();
+                context.closePath();
+            }
+        }
+
+        if ( res == 'up' || res == "out" ) {
+            flag = false;
+        }
+
+        if ( res == 'move' ) {
+            if (flag) {
+                prevX = currX;
+                prevY = currY;
+                currX = e.pageX - canvas.offsetLeft;
+                currY = e.pageY - canvas.offsetTop;
+                draw(color);
+            }
+        }
+    }
+    //------------------------------------------------------------------------
+    function draw( color ) {       
+        context.beginPath();
+        context.arc(currX, currY, 7.5, Math.PI*2, false);
+        context.lineWidth = slider();
+        context.strokeStyle = color;
+        context.stroke();
+        context.closePath();
+    }
+  }//FIN FUNCION DIBUJO/BORRAR
 
 
 });
