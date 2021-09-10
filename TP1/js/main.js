ae5c97906d41751bb71ef0d27f4bceb9d6769e84
Pedro <pedrochatelain@gmail.com>
2021-09-10 18:09:04 -0300

    Agrega filtro sepia

--- TP1/js/main.js
+++ TP1/js/main.js
@@ -11,11 +11,13 @@ function init() {
   const goma  = new Goma(2, document.querySelector('.js-eraser'), '#FFFFFF');
   const buttonUpload = document.querySelector('.js-button-upload');
   const filtroNegativo = new FiltroNegativo(document.querySelector('.js-filter-negativo'));
-  
+  const filtroSepia = new FiltroSepia(document.querySelector('.js-filter-sepia'));
+
   paint.listenButtonUpload(buttonUpload);
   paint.listenTool(lapiz);
   paint.listenTool(goma);
   paint.listenFilter(filtroNegativo);
+  paint.listenFilter(filtroSepia);
 }
 
 function showFilters(popUp) {
