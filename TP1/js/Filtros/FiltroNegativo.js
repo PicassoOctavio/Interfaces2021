598119f8c56ef348b6cb643988ec2feebe2e7f8d
Picasso Octavio <octaviopicasso.ma@gmail.com>
2021-09-12 22:38:55 -0300

    cambio menor

--- TP1/js/Filtros/FiltroNegativo.js
+++ TP1/js/Filtros/FiltroNegativo.js
@@ -10,8 +10,9 @@ class FiltroNegativo {
     let context = canvas.context;
     let imgData = context.getImageData(0, 0, canvas.width, canvas.height);
     let pixels = imgData.data;
+
     for (let i = 0; i < pixels.length; i += 4) {
-      pixels[i] = 255 - pixels[i]; // rojo
+      pixels[i] = 255 - pixels[i];         // rojo
       pixels[i + 1] = 255 - pixels[i + 1]; // verde
       pixels[i + 2] = 255 - pixels[i + 2]; // azul
     }
