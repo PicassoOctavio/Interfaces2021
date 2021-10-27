fefd1990ccb65135741a45008243f87d5e5bf70a
Pedro <pedrochatelain@gmail.com>
2021-10-26 22:30:50 -0300

    Add coin y animacion al tocarla

---
+++ TP3/js/Coin.js
@@ -0,0 +1,31 @@
+class Coin {
+
+  constructor(div) {
+    this.div = div;
+    this.body = div.querySelector('.js-coin-body');
+  }
+
+  overlaps(a, b) {
+    const rect1 = a.getBoundingClientRect();
+    const rect2 = b.getBoundingClientRect();
+    const isInHoriztonalBounds =
+      rect1.x < rect2.x + rect2.width && rect1.x + rect1.width > rect2.x;
+    const isInVerticalBounds =
+      rect1.y < rect2.y + rect2.height && rect1.y + rect1.height > rect2.y;
+    const isOverlapping = isInHoriztonalBounds && isInVerticalBounds;
+    return isOverlapping;
+  }
+
+  isTouched = (character) => {
+    return this.overlaps(this.body, character.body)
+  }
+
+  animate = () => {
+    this.div.classList.add("js-coin-collected");
+    this.div.addEventListener('animationend', () => {
+      this.div.classList.remove("js-coin-collected");
+    })
+
+  }
+
+}
--- TP3/js/main.js
+++ TP3/js/main.js
@@ -6,6 +6,10 @@ const character = new Character(characterElement);
 const blockElement = document.querySelector('.js-block');
 const block = new Block(blockElement);
 
+// Creo coin
+const coinElement = document.querySelector('.js-coin');
+const coin = new Coin(coinElement);
+
 // Checkea cada 10 milisegundos si el personaje chocó con el bloque
 // checkDead = () => {
 //   setInterval(function() {
@@ -15,5 +19,17 @@ const block = new Block(blockElement);
 //   }, 10);
 // }
 
+checkCoin = () => {
+  setInterval(function() {
+    if (coin.isTouched(character)) {
+      // updateScore();
+      coin.animate();
+    }
+  }, 10);
+}
+
+console.log(coin.body)
+
+document.addEventListener('DOMContentLoaded', checkCoin)
 document.addEventListener('DOMContentLoaded', checkDead)
 
