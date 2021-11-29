952533063dbf4788fb8850987c6d2ad0a9ac468c
Picasso Octavio <octaviopicasso.ma@gmail.com>
2021-11-29 08:39:11 -0300

    ajustes de mensaje + funcion sendMensaje

--- TP5/main.js
+++ TP5/main.js
@@ -12,3 +12,19 @@ function myFunction() {
 }
 
 
+/* ------------------------ CHAT ------------------------------ */
+
+function sendMessage(){
+  const mensaje = document.querySelector(".post-send-message").value;
+  let columnMensaje = document.querySelector("messages");
+  let divMensaje = document.createElement('div');
+  const textoMensaje = document.createElement('p');
+
+  divMensaje.className = "message-sended";
+  textoMensaje.className = "post-message-sended-chat";
+  divMensaje.appendChild(textoMensaje);
+  columnMensaje.appendChild(columnMensaje);
+  
+}
+
+
