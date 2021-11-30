4d5c97961258210ff4ecc1137297ad52a27f47a1
Picasso Octavio <octaviopicasso.ma@gmail.com>
2021-11-29 21:19:00 -0300

    envio de mensaje en chat

--- TP5/main.js
+++ TP5/main.js
@@ -14,17 +14,67 @@ function myFunction() {
 
 /* ------------------------ CHAT ------------------------------ */
 
-function sendMessage(){
-  const mensaje = document.querySelector(".post-send-message").value;
-  let columnMensaje = document.querySelector("messages");
-  let divMensaje = document.createElement('div');
-  const textoMensaje = document.createElement('p');
+const btnSendMessage = document.querySelector(".btnSendMessage-js");
+btnSendMessage.addEventListener("click", sendMessage)
 
-  divMensaje.className = "message-sended";
-  textoMensaje.className = "post-message-sended-chat";
-  divMensaje.appendChild(textoMensaje);
-  columnMensaje.appendChild(columnMensaje);
+function sendMessage(){
+  let mensaje = document.querySelector(".post-send-message").value;
+  if ( mensaje ){
+    let columnMensaje = document.querySelector(".messages");
+    let divMensaje = document.createElement('div');
+    let textoMensaje = document.createElement('p');
+    let imgAvatar = document.createElement('img');
   
+    imgAvatar.className = "post-chat-user-icon-sended";
+    imgAvatar.src = "img/userIcon.svg";
+    divMensaje.className = "message-sended";
+    textoMensaje.className = "post-message-sended-chat";
+    
+    console.log(imgAvatar);
+    textoMensaje.innerHTML = mensaje;
+    divMensaje.appendChild(textoMensaje);
+    divMensaje.appendChild(imgAvatar);
+    columnMensaje.appendChild(divMensaje);
+  
+    document.querySelector(".post-send-message").value = null;
+    document.querySelector(".post-send-message").focus();
+  }
 }
+
+function loadImage() {
+
+}
+
+/* 
+listenButtonUpload(button) {
+    button.addEventListener('click', async () => {
+      let inputFile = document.querySelector('.js-input-file');
+      inputFile.click();
+      let image = await this.getImage(inputFile);
+      inputFile.value = '';
+      this.canvas.whiten();
+      this.canvas.drawImage(image);
+      this.uncheckFilters();
+    })
+  }
+*/
+
+/* 
+  getImage(inputFile) {
+    return new Promise((resolve, reject) => {
+      inputFile.onchange = e => {
+        let file = e.target.files[0];
+        let reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = readerEvent => {
+          let content = readerEvent.target.result;
+          let image = new Image();
+          image.src = content;
+          image.onload = () => { resolve (image); }
+        }
+      }
+    })
+  }
+*/
 
 
