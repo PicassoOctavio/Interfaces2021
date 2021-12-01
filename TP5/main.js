87dc60c0f9e35f8de1074a28f64b5747180f368a
Pedro <pedrochatelain@gmail.com>
2021-12-01 18:23:57 -0300

    Add comportamiento al botón filtros

--- TP5/main.js
+++ TP5/main.js
@@ -14,10 +14,39 @@ function myFunction() {
 
 const inputBuscador = document.querySelector('.js-input-buscador');
 const btnCloseBusquedasRecientes = document.querySelector('.js-btn-cerrar-busquedas');
+const btnFiltrosBusqueda = document.querySelector('.js-btn-filtros-busqueda');
+const btnLimpiarFiltros = document.querySelector('.js-btn-limpiar-filtros');
+const filtros = document.querySelectorAll('.js-filtro');
+
+if (filtros) {
+  filtros.forEach(filtro => {
+    filtro.addEventListener('click', () => {
+      if (filtro.checked == true) {
+        updateBadge(+1);
+      } else {
+        updateBadge(-1);
+      }
+    })
+  });
+}
+
+if (btnFiltrosBusqueda) {
+  btnFiltrosBusqueda.addEventListener('click', () => {
+    mostrarFiltrosDeBusqueda();
+    ocultarBusquedasRecientes();
+  })
+}
+
+if (btnLimpiarFiltros) {
+  btnLimpiarFiltros.addEventListener('click', () => {
+    limpiarFiltros(filtros);
+  })
+}
 
 if (inputBuscador) {
   inputBuscador.addEventListener('focus', () => {
     mostrarBusquedasRecientes();
+    ocultarFiltrosDeBusqueda();
   })
   
   inputBuscador.addEventListener('keyup', () => {
@@ -35,6 +64,30 @@ if (btnCloseBusquedasRecientes) {
   btnCloseBusquedasRecientes.addEventListener('click', ocultarBusquedasRecientes)
 }
 
+function updateBadge(nro) {
+  console.log(nro)
+  const spanDelBadge = document.querySelector('.js-span-badge');
+  let nroDelBadge = parseInt(spanDelBadge.innerHTML);
+  spanDelBadge.innerHTML = nroDelBadge + nro;
+}
+
+function limpiarFiltros(filtros) {
+  filtros.forEach(filtro => {
+    filtro.checked = false;
+  });
+  const spanDelBadge = document.querySelector('.js-span-badge');
+  spanDelBadge.innerHTML = 0;
+}
+
+function mostrarFiltrosDeBusqueda() {
+  const filtrosBusqueda = document.querySelector('.js-filtros-busqueda');
+  filtrosBusqueda.classList.toggle('js-display-none');
+}
+
+function ocultarFiltrosDeBusqueda() {
+  const filtrosBusqueda = document.querySelector('.js-filtros-busqueda');
+  filtrosBusqueda.classList.add('js-display-none');
+}
 
 
 function mostrarBusquedasRecientes() {
