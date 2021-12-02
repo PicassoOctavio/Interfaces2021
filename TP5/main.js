3c5c91c099a796a05b76ae0a1b75d706417cd2fd
Pedro <pedrochatelain@gmail.com>
2021-12-02 16:56:31 -0300

    Add comportamiento al buscador

--- TP5/main.js
+++ TP5/main.js
@@ -17,6 +17,17 @@ const btnCloseBusquedasRecientes = document.querySelector('.js-btn-cerrar-busque
 const btnFiltrosBusqueda = document.querySelector('.js-btn-filtros-busqueda');
 const btnLimpiarFiltros = document.querySelector('.js-btn-limpiar-filtros');
 const filtros = document.querySelectorAll('.js-filtro');
+const btnLimpiarBuscador = document.querySelector('.js-btn-limpiar-buscador');
+
+if (btnLimpiarBuscador) {
+  btnLimpiarBuscador.addEventListener('click', () => {
+    inputBuscador.value = '';
+    ocultarBotonLimpiarBuscador();
+    ocultarResultado();
+    inputBuscador.focus();
+  })
+}
+
 
 if (filtros) {
   filtros.forEach(filtro => {
@@ -45,23 +56,79 @@ if (btnLimpiarFiltros) {
 
 if (inputBuscador) {
   inputBuscador.addEventListener('focus', () => {
-    mostrarBusquedasRecientes();
+    if ( ! inputBuscador.value) {
+      mostrarBusquedasRecientes();
+      ocultarResultado();
+    } else {
+      mostrarBotonLimpiarBuscador();
+    }
     ocultarFiltrosDeBusqueda();
   })
   
   inputBuscador.addEventListener('keyup', () => {
     if (inputBuscador.value != '') {
+      mostrarBotonLimpiarBuscador();
       ocultarBusquedasRecientes();
+      mostrarBuscando();
+      ocultarResultado();
+      let interval = setInterval(() => {
+        ocultarBuscando();
+        if (inputBuscador.value != '') {
+          mostrarResultado(inputBuscador.value);
+        }
+      }, 1000);
+      setInterval(() => {
+        clearInterval(interval)
+      }, 1000);
     } else {
+      ocultarResultado();
+      ocultarBotonLimpiarBuscador();
       mostrarBusquedasRecientes();
     }
   
     console.log(inputBuscador.value)
   })
+
 }
+
 
 if (btnCloseBusquedasRecientes) {
   btnCloseBusquedasRecientes.addEventListener('click', ocultarBusquedasRecientes)
+}
+
+function mostrarBotonLimpiarBuscador() {
+  btnLimpiarBuscador.classList.remove('js-display-none');
+}
+
+function ocultarBotonLimpiarBuscador() {
+  btnLimpiarBuscador.classList.add('js-display-none')
+}
+
+
+function ocultarResultado() {
+  const resultados = document.querySelector('.js-resultados-busqueda')
+  // const h1 = document.querySelector('.js-titulo-resultados');
+  
+  resultados.classList.add('js-display-none');
+  // h1.innerHTML = res;
+}
+
+function mostrarBuscando() {
+  const buscando = document.querySelector('.js-buscando');
+  buscando.classList.remove('js-display-none');
+}
+
+function ocultarBuscando() {
+  const buscando = document.querySelector('.js-buscando');
+  buscando.classList.add('js-display-none');
+}
+
+function mostrarResultado(res) {
+  const resultados = document.querySelector('.js-resultados-busqueda')
+  const h1 = document.querySelector('.js-titulo-resultados');
+  
+  resultados.classList.remove('js-display-none');
+  h1.innerHTML = res;
 }
 
 function updateBadge(nro) {
